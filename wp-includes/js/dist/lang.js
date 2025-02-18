f8d11b00b1e6785d8c894f2f9cb8a6576619f772
Markl1n1 <151952963+Markl1n1@users.noreply.github.com>
2025-02-19 01:41:50 +0200

    -

--- wp-includes/js/dist/lang.js
+++ wp-includes/js/dist/lang.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById("language-modal");
     const languageLinks = document.querySelectorAll(".language-options a");
+    const languageBtn = document.getElementById("language-btn");
 
     // Check if a language is already saved in localStorage
     const savedLang = localStorage.getItem("userLanguage");
     if (!savedLang) {
-        modal.style.display = "flex"; // Show the modal if no language is saved
-    } else {
-        // Redirect to saved language page if necessary
-        if (!window.location.href.includes(`/${savedLang}/`)) {
-            window.location.href = `https://mln.finance/${savedLang}/`;
-        }
+        modal.style.display = "flex"; // Show modal on first visit
     }
+
+    // Click event to open modal when button is clicked
+    languageBtn.addEventListener("click", function () {
+        modal.style.display = "flex";
+    });
 
     // Handle language selection
     languageLinks.forEach(link => {
